07522f87918e66b75ddbf51c320c4bebf07912c5
Cory Jett <cjett@pivotal.io>
2020-07-23 10:23:51 -0500

    Adds POST functionality

--- client/index.js
+++ client/index.js
@@ -1,12 +1,17 @@
 const loadtest = require('loadtest');
 var cfenv = require("cfenv")
+const opentracing = require('opentracing');
+const tracer = new opentracing.Tracer();
+
+const span = tracer.startSpan('http_request');
 
 var appEnv = cfenv.getAppEnv()
 
 function getParams(){
     var endpoint = process.env.endpoint ? process.env.endpoint : 'http://google.com';
-    var maxRequests = process.env.maxRequests? process.env.maxRequests : null;
-    return {endpoint: endpoint, maxRequests: maxRequests}
+    var maxRequests = process.env.maxRequests ? process.env.maxRequests : null;
+    var rps = process.env.rps ? process.env.rps : 50;
+    return {endpoint: endpoint, maxRequests: maxRequests, rps: rps}
 }
 
 function statusCallback(error, result, latency) {
@@ -21,11 +26,26 @@ const options = {
     url: getParams().endpoint,
     maxRequests: getParams().maxRequests,
     insecure: true,
+    requestsPerSecond: getParams().rps,
+    method: 'POST',
+    body:'',
     statusCallback: statusCallback,
+    requestGenerator: (params, options, client, callback) => {
+		const message = '{"Message"}';
+		options.headers['Content-Length'] = message.length;
+		options.headers['Content-Type'] = 'text/plain';
+		options.body = 'Data';
+		options.path = '/';
+		const request = client(options, callback);
+		request.write(message);
+		return request;
+	}
 };
 
 console.log("endpoint: "+ getParams().endpoint)
 console.log("maxRequsts: "+ getParams().maxRequests)
+
+
 
 loadtest.loadTest(options, function(error, result)
 {
--- server/index.js
+++ server/index.js
@@ -1,7 +1,56 @@
 var http = require('http');
+var querystring = require('querystring');
 
-http.createServer(function (req, res) {
-    res.write('Hello World!');
-    res.end();
-  }).listen(process.env.port || 8080, () => {
+function processPost(request, response, callback) {
+  var queryData = "";
+  if(typeof callback !== 'function') return null;
+
+  if(request.method == 'POST') {
+      request.on('data', function(data) {
+          queryData += data;
+          if(queryData.length > 1e6) {
+              queryData = "";
+              response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+              request.connection.destroy();
+          }
+      });
+
+      request.on('end', function() {
+          request.post = querystring.parse(queryData);
+          callback();
+      });
+
+  } else {
+      response.writeHead(405, {'Content-Type': 'text/plain'});
+      response.end();
+  }
+}
+
+http.createServer(function (request, response) {
+  if(request.method == 'POST') {
+    try{
+      processPost(request, response, function() {
+        console.log(request.post);
+        response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+        response.end();
+    })
+    }catch (error){
+      console.error(console.error(error))
+    }
+} else {
+    response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+    response.write("Got it!")
+    response.end();
+}
+}).listen(process.env.port || 8080, () => {
     console.log(`Server is running on 8080`)})
+
+// http.createServer((request, response) => {
+//   console.log('Now we have a http message with headers but no data yet.');
+//   request.on('data', chunk => {
+//     console.log('A chunk of data has arrived: ', chunk);
+//   });
+//   request.on('end', () => {
+//     console.log('No more data');
+//   })
+// }).listen(8080)
